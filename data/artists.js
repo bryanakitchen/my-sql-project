3e25cd52f984b7f5e6c712f5bb596603b18621b9
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-11-02 21:08:11 -0500

    updated to new data

---
+++ data/artists.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    name: 'Griz',
+    first_album: 2011,
+    on_tour: false,
+    genre: 'electronica',
+  },
+  {
+    name: 'Odesza',
+    first_album: 2012,
+    on_tour: false,
+    genre: 'indietronica',
+  },
+  {
+    name: 'Disclosure',
+    first_album: 2013,
+    on_tour: false,
+    genre: 'house',
+  },
+  {
+    name: 'Louis the Child',
+    first_album: 2013,
+    on_tour: false,
+    genre: 'dance pop',
+  },
+];
---
+++ data/load-seed-data.js
@@ -0,0 +1,47 @@
+const client = require('../lib/client');
+// import our seed data:
+const artists = require('./artists.js');
+const usersData = require('./users.js');
+const { getEmoji } = require('../lib/emoji.js');
+
+run();
+
+async function run() {
+
+  try {
+    await client.connect();
+
+    const users = await Promise.all(
+      usersData.map(user => {
+        return client.query(`
+                      INSERT INTO users (email, hash)
+                      VALUES ($1, $2)
+                      RETURNING *;
+                  `,
+        [user.email, user.hash]);
+      })
+    );
+      
+    const user = users[0].rows[0];
+
+    await Promise.all(
+      artists.map(artist => {
+        return client.query(`
+                    INSERT INTO artists (name, first_album, on_tour, genre, owner_id)
+                    VALUES ($1, $2, $3, $4, $5);
+                `,
+        [artist.name, artist.first_album, artist.on_tour, artist.genre, user.id]);
+      })
+    );
+    
+
+    console.log('seed data load complete', getEmoji(), getEmoji(), getEmoji());
+  }
+  catch(err) {
+    console.log(err);
+  }
+  finally {
+    client.end();
+  }
+    
+}
